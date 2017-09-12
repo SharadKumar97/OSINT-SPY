240d11557ef7fbce8fe8f702178295075e57ecee
SharadKumar97 <sharadkumar98780@gmail.com>
2017-09-12 11:15:01 -0700

    Update config.py

--- modules/config.py
+++ modules/config.py
@@ -6,7 +6,7 @@ global clearbit_api_key
 global shodan_api_key
 global fullcontact_api_key
 global virus_total_api_key
-global email_hunter_api
+global email_hunter_api_key
 
 clearbit_api_key=" "
 shodan_api_key=" "
