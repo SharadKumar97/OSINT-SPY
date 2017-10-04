916d89ea373ec6a57aa9228e4eb378717cd14bd4
Manuel Moya <moya.manuel41@gmail.com>
2017-10-04 11:00:20 -0700

    Merge Windows and *nix setup files

--- setup.py
+++ setup.py
@@ -1,1 +1,19 @@
+import os
+import platform
 
+if (platform.system() != "Windows"):
+    os.system('sudo apt-get install python-pip')
+    os.system('sudo pip install clearbit')
+    os.system('sudo pip install shodan')
+    os.system('sudo pip install fullcontact.py')
+    os.system('sudo pip install steganography')
+    os.system('sudo pip install requests')
+    os.system('sudo pip install sslscan')
+    os.system('sudo pip install pythonwhois')
+else:
+    os.system('C:\Python27\Scripts\pip install clearbit')
+    os.system('C:\Python27\Scripts\pip install shodan')
+    os.system('C:\Python27\Scripts\pip install fullcontact.py')
+    os.system('C:\Python27\Scripts\pip install steganography')
+    os.system('C:\Python27\Scripts\pip install requests')
+    os.system('C:\Python27\Scripts\pip install sslscan') 
