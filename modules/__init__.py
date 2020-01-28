fccadfd89211e2054d2ef6889a4f97237ff5f016
Sayam753 <sayamkumar753@yahoo.in>
2020-01-28 12:18:35 +0530

    Delete unwanted files

--- modules/__init__.py
+++
@@ -1,7 +0,0 @@
-from .bitcoin import *
-from .company_detail import *
-from .device_search import *
-from .email_search import *
-from .ip_enum import *
-from .malware import *
-from .ssl_scan import *
--- modules/config.py
+++
@@ -1,9 +0,0 @@
-##############################
-#     Paste API Key's Here
-##############################
-
-clearbit_api_key = ""
-shodan_api_key = ""
-fullcontact_api_key = ""
-virus_total_api_key = ""
-email_hunter_api_key = ""
